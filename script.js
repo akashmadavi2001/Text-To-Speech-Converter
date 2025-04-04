9db4a9899abd06b2d8038d83029e14375ceb3d62
Akash Madavi <akashmadavi2001@gmail.com>
2025-04-04 21:42:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let speech = new SpeechSynthesisUtterance();
-
-document.querySelector('button').addEventListener('click', () => {
+let btn = document.querySelector('button');
+btn.addEventListener('click', () => {
     speech.text = document.querySelector('textarea').value;
     window.speechSynthesis.speak(speech);
 });
